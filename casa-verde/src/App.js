ca4b421c31c13cbd6a3bd86522223a06253d7be6
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2023-06-28 16:51:18 +0100

    Completed header

--- casa-verde/src/App.js
+++ casa-verde/src/App.js
@@ -10,10 +10,10 @@ function App() {
     <div className="App">
       <Banner />
 
-      <div className="header mt-8 mb-16 flex justify-end">
-        <h1 className="logo text-[#3CAEA3] w-1/2 my-auto font-sans font-bold text-6xl">Casa Verde</h1>
+      <div className="header mt-8 mb-8 flex justify-end lg:mb-18">
+        <h1 className="logo text-[#3CAEA3] text-3xl w-1/2 ml-4 my-auto font-sans font-bold lg:text-6xl">Casa Verde</h1>
         <SearchBox />
-        <img src="/images/cart3.png" alt="Product carts" className="ml-8 my-auto mr-14 max-mt-16 lg:w-12 lg:h-12"></img>
+        <img src="/images/cart3.png" alt="Product carts" className="ml-8 my-auto mr-4 lg:mr-14 max-mt-16 w-10 lg:w-12 lg:h-12"></img>
       </div>
 
       <div className="products-gallery">
--- casa-verde/src/SearchBox.js
+++ casa-verde/src/SearchBox.js
@@ -2,13 +2,21 @@ import React from 'react'
 
 function SearchBox() {
   return (
-    <div className="border border-black my-auto px-4 py-2 bg-[#D9D9D936] lg:w-96 lg:h-14">
-        
-        <form className="flex">
-            <input type="text" placeholder="" name="search" className="w-full outline-none bg-transparent" />
-                <button type="submit"><i className="fa fa-search" style={{fontSize:"36px", color:"#3CAEA3"}}></i></button>
-        </form>
+    <div className="my-auto">
+
+      <div className="hidden border border-black my-auto px-4 py-2 bg-[#D9D9D936] md:block lg:w-96 lg:h-14">
+          <form className="flex">
+              <input type="text" placeholder="" name="search" className="w-full outline-none bg-transparent" />
+                  <button type="submit"><i className="fa fa-search" style={{fontSize:"36px", color:"#3CAEA3"}}></i></button>
+          </form>
+      </div>
+      <div className="align-middle ml-12 md:hidden text-center">
+        <button type="submit"><i className="fa fa-search" style={{fontSize:"36px", color:"#3CAEA3",}}></i></button>
+      </div>
+
     </div>
+
+    
     
   )
 }
